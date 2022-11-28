7efe30e9ce4fa6024ce0cd13913a82981113691d
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-28 19:42:34 +0100

    Aggiunge CharacterCard grezza

---
+++ src/components/main/CharacterCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'CharacterCard',
+}
+</script>
+
+<template>
+   <div class="card text-center p-3 m-3">
+        <img class="img-fluid" src="https://via.placeholder.com/190x275" alt="">
+        <h5>NAME CHARACTER</h5>
+        <p>Category</p>
+        <span>Status</span>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    background-color: #2e3a46;
+}
+</style>
--- src/components/main/SectionCharacters.vue
+++ src/components/main/SectionCharacters.vue
@@ -1,6 +1,11 @@
 <script>
+import CharacterCard from './CharacterCard.vue';
+
 export default {
     name: 'SectionCharacters',
+    components: {
+        CharacterCard,
+    }
 }
 </script>
 
@@ -8,6 +13,9 @@ export default {
     <section id="characters" class="p-5">
         <div class="found-characters ps-3 py-4">
             <span class="fw-bold">Found 62 characters</span>
+        </div>
+        <div class="d-flex">
+            <CharacterCard/>
         </div>
     </section>
 </template>
