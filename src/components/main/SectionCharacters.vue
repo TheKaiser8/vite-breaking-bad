42f80fed3a7e55b2fcabeaf3460941778610ed66
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-28 19:17:56 +0100

    Aggiunge found-characters in SectionCharacters

--- src/components/main/SectionCharacters.vue
+++ src/components/main/SectionCharacters.vue
@@ -5,14 +5,21 @@ export default {
 </script>
 
 <template>
-    <section id="characters">
-
+    <section id="characters" class="p-5">
+        <div class="found-characters ps-3 py-4">
+            <span class="fw-bold">Found 62 characters</span>
+        </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
 #characters {
     background-color: #ffffff;
-    height: 500px // debug
+    color: #fff;
+    
+    .found-characters {
+        background-color: #212529;
+    }
+
 }
 </style>
